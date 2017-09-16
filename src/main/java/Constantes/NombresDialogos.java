ea04b5aaccfeb1397ac2fe6407632d293b415b3c
matias <matiasrh84@gmail.com>
2017-09-15 21:01:29 -0300

    primerda subida

---
+++ src/main/java/Constantes/NombresDialogos.java
@@ -0,0 +1,25 @@
+package Constantes;
+
+public class NombresDialogos {
+
+    public static final String ALUMNOS = "Alumnos";
+    public static final String ASIGNAR_FECHA_LIBRO_FOLIO = "Fecha libro y folio";
+    public static final String ASIGNAR_NOTAS_PREVIAS = "Asignar notas previas";
+    public static final String COMPLETAR_TP = "Completar TP";
+    public static final String ASIGNATURAS = "Asignaturas";
+    public static final String CARRERAS = "Carreras";
+    public static final String CURSOS = "Cursos";
+    public static final String FAMILIARES = "Familiares";
+    public static final String IMPRESION_DE_LIBRETAS = "Impresión de libretas";
+    public static final String INASISTENCIAS = "Inasistencias";
+    public static final String JUSTIFICAR_INASISTENCIAS = "Justificar Inasistencias";
+    public static final String LOGIN = "Iniciar Sesión";
+    public static final String MAIN = "GIT";
+    public static final String NOTAS = "Notas";
+    public static final String NOTA = "Nota";
+    public static final String CONSULTA_ALUMNOS = "Detalle de notas";
+    public static final String PROFESORES = "Profesores";
+    public static final String SANCIONES = "Registrar sanciones";
+    public static final String CREAR_TP = "Trabajos Prácticos";
+
+}
